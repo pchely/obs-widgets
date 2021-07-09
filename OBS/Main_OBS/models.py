403b45681ccdec45de12183f3adebd6fabd6cab9
Leo <azat.abdrashitov7@gmail.com>
2021-07-09 17:51:19 +0300

    fix and update models

--- OBS/Main_OBS/models.py
+++ OBS/Main_OBS/models.py
@@ -4,7 +4,6 @@ class OBS_Model(models.Model):
     name = models.CharField(max_length=255, unique=True)
     file = models.FileField(upload_to="files/")
     slug = models.SlugField(max_length=50, unique=True, null=True, verbose_name="URL")
-    time_create = models.DateTimeField(auto_now_add=True)
-
-    def get_absolute_url(self):
-        return reversed("widget", kwargs={"name_slug":self.slug})
+    status = models.BooleanField(default=True)
+    title = models.CharField(max_length=50, null=True);
+    subtitle = models.CharField(max_length=50, null=True);
