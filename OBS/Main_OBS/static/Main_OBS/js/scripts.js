f0d28141b766f8610586fc3e8e36bb699b9b048d
Leo <azat.abdrashitov7@gmail.com>
2021-07-09 17:56:48 +0300

    add show/hide widget and some functional

--- OBS/Main_OBS/static/Main_OBS/js/scripts.js
+++ OBS/Main_OBS/static/Main_OBS/js/scripts.js
@@ -1,9 +1,35 @@
-function Widget(obj) {
-  let name = document.getElementById(obj.id).innerText;
-  // alert(name);
+function Open_Widget() {
+  let name = document.getElementById("list_select").value;
   let link = window.open('http://127.0.0.1:8000/widget/'+name, '_blank');
-  link.onload = function () {
-    global_value = link;
-    console.log(global_value);
+};
+
+
+function Show_Hide_Widget() {
+    let name = document.getElementById('list_select').value;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:8000/edit_status/"+name);
+    xhr.send();
+  };
+
+
+function Delete() {
+  let name = document.getElementById("list_select").value;
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://127.0.0.1:8000/delete/"+name);
+  xhr.send();
+  parent.location.reload();
+};
+
+
+function Show_Info(obj) {
+  let name = obj.innerText;
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "http://127.0.0.1:8000/about_widget/"+name);
+  xhr.responseType = 'json';
+  xhr.send();
+  xhr.onload = function () {
+    console.log("Widget:", name);
+    console.log("Title:", xhr.response.title);
+    console.log("Subtitle:", xhr.response.subtitle)
   }
-}
+};
