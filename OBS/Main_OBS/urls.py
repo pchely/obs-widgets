a5dd1179984a74f4e5749651681b9e698cf5390b
Leo <azat.abdrashitov7@gmail.com>
2021-07-09 17:56:21 +0300

    upd Main_OBS urls

--- OBS/Main_OBS/urls.py
+++ OBS/Main_OBS/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin_board/', views.admin_board, name="home"),
-    path('widget/<slug:name_slug>/', views.open_widget, name="widget")
+    path('edit_widget/', views.edit_widget, name="edit_widget"),
+    path('about_widget/<slug:name_slug>/',views.ShowInfo.as_view()),
+    path('edit_status/<slug:name_slug>/', views.edit_status),
+    path('check_status/<slug:name_slug>/', views.CheckStatus.as_view(), name="showhide"),
+    path('delete/<slug:slug_file>/', views.delete_widget, name="delete"),
+    path('widget/<slug:name_slug>/', views.open_widget, name="widget"),
+    path('admin_board/', views.admin_board, name="home")
 ]
