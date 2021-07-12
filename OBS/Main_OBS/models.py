76cb642e5b3f966958c978a3447ad8beb073b26c
Leo <azat.abdrashitov7@gmail.com>
2021-07-12 16:25:13 +0300

    fixed models

--- OBS/Main_OBS/models.py
+++ OBS/Main_OBS/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class OBS_Model(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=50, unique=True, verbose_name="Name")
+    # name = models.CharField(max_length=255, unique=True)
     file = models.FileField(upload_to="files/")
-    slug = models.SlugField(max_length=50, unique=True, null=True, verbose_name="URL")
-    status = models.BooleanField(default=True)
+    status = models.BooleanField(default=False)
     title = models.CharField(max_length=50, null=True);
     subtitle = models.CharField(max_length=50, null=True);
