2267a4f0a5da837067304668c592015fd44761bd
Leo <azat.abdrashitov7@gmail.com>
2021-07-13 17:49:34 +0300

    upd script

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-python -m venv venv
-CALL .\venv\Scripts\activate.bat
-python pip install -r requirements.txt
-echo -n makemigrations migrate | xargs -n 1 -d " " python manage.py
+python -m venv venv>>log.txt
+CALL .\venv\Scripts\activate.bat>>log.txt
+python pip install -r requirements.txt>>log.txt
+echo -n makemigrations migrate | xargs -n 1 -d " " python manage.py>>log.txt
