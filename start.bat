4cc7546be108f30a8b626496dbb36a9b56ee86e2
Leo <azat.abdrashitov7@gmail.com>
2021-07-13 17:41:27 +0300

    upd script

--- start.bat
+++ start.bat
@@ -2,6 +2,4 @@
 python -m venv venv
 CALL .\venv\Scripts\activate.bat
 python pip install -r requirements.txt
-python OBS\manage.py makemigrations
-python OBS\manage.py migrate
-python OBS\manage.py runserver
+echo -n makemigrations migrate | xargs -n 1 -d " " python manage.py
