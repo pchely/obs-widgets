5041d8777fe27e628b2232621d48ad11b55e2d3c
Leo <azat.abdrashitov7@gmail.com>
2021-07-13 17:24:42 +0300

    upd script

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
 python -m venv venv
 CALL .\venv\Scripts\activate.bat
+python pip install -r requirements.txt
 python OBS\manage.py makemigrations
 python OBS\manage.py migrate
 python OBS\manage.py runserver
